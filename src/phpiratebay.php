f67e8f9e3a0b7e53e6de8c4c74498b91e8e23423
mario.martinez <mario.martinez@seccionamarilla.com>
2018-10-24 16:29:53 -0500

    developing

--- src/phpiratebay.php
+++ src/phpiratebay.php
@@ -6,54 +6,46 @@
 		// 	
 		public $list = array();
 		public $theMirror = "";
+		private $searchTerms = null; // null search
+		private $searchCategory = 0; // defaults to all categories
+		private $order=3; // sort by date desc 
+		private $searchPage = 0;
 		private $categories = array(
-			"audio"=>100, 
-			"video"=>200, 
-			"applications"=>300,
-			"applications/windows"=>301,
-			"applications/android"=>306,
-			"games"=>400, 
-			"xxx"=>500, 
-			"other"=>600 
+			"audio"=>100, "audio/music"=>101, "audio/books"=>102, "audio/clips"=>103, "audio/flac"=>104, "audio/other"=>199, 
+			"video"=>200, "video/movies"=>201, "video/dvdr"=>202, "video/music"=>203, "video/clips"=>204, "video/tvshows"=>205, "video/handheld"=>206, "video/movies/hd"=>207, "video/tvshows/hd"=>208,	"video/3d"=>209, "video/other"=>299, 
+			"applications"=>300, "applications/windows"=>301, "applications/mac"=>302, "applications/unix"=>303, "applications/handheld"=>304, "applications/ios"=>305, "applications/android"=>306, "applications/other"=>399,
+			"games"=>400, "games/pc"=>401, "games/mac"=>402, "games/psx"=>403, "games/xbox"=>404, "games/wii"=>405, "games/handheld"=>406, "games/ios"=>407, "games/android"=>408, "games/other"=>499, 
+			"pr0n"=>500, "pr0n/movies"=>501, "pr0n/movies/dvdr"=>502, "pr0n/pictures"=>503, "pr0n/games"=>504, "pr0n/movies/hd"=>505, "pr0n/clips"=>506, "pr0n/other"=>599, 
+			"other"=>600, "other/ebook"=>601, "other/comics"=>602, "other/pictures"=>603, "other/covers"=>604, "other/physibles"=>605, "other/other"=>699
 		);
-/***
-<option value="0">All</option>
-<optgroup label="Audio"><option value="101">Music</option><option value="102">Audio books</option><option value="103">Sound clips</option><option value="104">FLAC</option><option value="199">Other</option></optgroup><optgroup label="Video"><option value="201">Movies</option><option value="202">Movies DVDR</option><option value="203">Music videos</option><option value="204">Movie clips</option><option value="205">TV shows</option><option value="206">Handheld</option><option value="207">HD - Movies</option><option value="208">HD - TV shows</option><option value="209">3D</option><option value="299">Other</option></optgroup><optgroup label="Applications"><option value="301">Windows</option><option value="302">Mac</option><option value="303">UNIX</option><option value="304">Handheld</option><option value="305">IOS (iPad/iPhone)</option><option value="306">Android</option><option value="399">Other OS</option></optgroup><optgroup label="Games"><option value="401">PC</option><option value="402">Mac</option><option value="403">PSx</option><option value="404">XBOX360</option><option value="405">Wii</option><option value="406">Handheld</option><option value="407">IOS (iPad/iPhone)</option><option value="408">Android</option><option value="499">Other</option></optgroup><optgroup label="Porn"><option value="501">Movies</option><option value="502">Movies DVDR</option><option value="503">Pictures</option><option value="504">Games</option><option value="505">HD - Movies</option><option value="506">Movie clips</option><option value="599">Other</option></optgroup><optgroup label="Other"><option value="601">E-books</option><option value="602">Comics</option><option value="603">Pictures</option><option value="604">Covers</option><option value="605">Physibles</option><option value="699">Other</option></optgroup></select>
-*/
-
-
-
+		/**
+			 curl get contents
+		*/
+		private $curlTimeout=5;
+		private $curlProxy=null;		
 		/*
 		Proxy List:
 			https://thepiratebay-proxylist.se/api/v1/proxies	 
-		Query:
-			https://alivbay.org/s/?q=apk&page=0&orderby=99
-		opts: 
-			category:
-				0 : all
-				101: 699
-			page:
-				0-99
-			orderBy:
-				1 name desc
-				2 name asc
-				3 date desc
-				4 date asc
-				5 size desc
-				6 size asc
-				7 seeds desc
-				8 seeds asc
-				9 leeches desc
-				10 leeches asc
-		*/		
-
-		/***
-		* 
-		* 
-		*
+											  P S Cat
+			https://xxxxxxxxxxx/search/empire/0/7/200
 		*/
-
-
+		private function curl_get_contents($url=null){
+			$ch = curl_init();
+			if($ch === false || $url==null)
+			{
+			    return "curl NOK obj";
+			}
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, is_numeric($this->curlTimeout)?$this->curlTimeout:5);
+			//proxy details
+			if($this->curlProxy!=null)
+				curl_setopt($ch, CURLOPT_PROXY, $this->curlProxy);
+			$data = curl_exec($ch);
+			echo $data;
+			curl_close($ch);
+			return $data;
+		}		
 		public function __construct($searchTerm=array(), $category="all", $orderBy="dateDesc", $page=0, $mirror = null ){
 			if ($mirror == null){
 				$tmp = $this->getProxyList()->proxies;
@@ -72,7 +64,67 @@
 			$this->list = json_decode($o);
 			return $this->list;
 		}
-		
+
+		public function getCategories(){
+			$o = "";
+			foreach ($this->categories as $k=>$v){
+				$o .= "$k:$v, ";
+			}
+			$o = rtrim($o, ", ");
+			return $o;
+		}
+		public function searchFor($what=null){
+			$this->searchTerms = $what;
+		}
+		public function searchInCategory($cat){
+			$cat = trim($cat);
+			if($this->categories[$cat]>0){
+				$this->searchCategory = $this->categories["$cat"];
+				return true;
+			}else{
+				return false;
+			}
+		}
+		public function orderBy($field="date", $order="desc"){
+			switch(strtolower($field)){
+				case "name":
+					$this->order = ($order == "desc")?1:2;
+					break;
+				case "date":
+					$this->order = ($order == "desc")?3:4;
+					break;
+				case "size":
+					$this->order = ($order == "desc")?5:6;
+					break;
+				case "seeds":
+					$this->order = ($order == "desc")?7:8;
+					break;
+				case "leeches":
+					$this->order = ($order == "desc")?9:10;
+					break;
+				default:
+					return false;
+					break;
+			}
+			return true;
+		}
+		public function getSearchUrl(){
+			$o = "$this->theMirror/search/$this->searchTerms/$this->searchPage/$this->order/$this->searchCategory";
+			return $o;
+		}
+
+		public function doSearchUrl(){
+			$url = $this->getSearchUrl();
+			return $this->curl_get_contents($url);
+		}
+
+		public function setProxy($proxy){
+			if($proxy==null){
+				$this->curlProxy = null;
+			}else{
+				$this->curlProxy = $proxy;
+			}	
+		}
 	}
 	/////////////////////////
 
@@ -81,6 +133,14 @@
 	//	$opts->option('m')->aka('man')->describedAs(MAN)->boolean()->defaultsTo(false);
 	//	$opts->option('e')->aka('engine')->describedAs('--engine <...> or -e <google|duckgo|searxme|playstore> or  -e <g|d|s|p>')->defaultsTo("s");
 	$pb = new phpiratebay();
-	echo $pb->theMirror;
+	// $pb->theMirror;
+	// echo $pb->getCategories();
 	// print_r($pb->list);
+//	$pb->setProxy("socks5://localhost:9999"); //not working ??
+//	$pb->setProxy("http://127.0.0.1:9999"); //not working ??
+	$pb->searchFor("empire strikes back");
+	$pb->searchInCategory("video");
+	$pb->orderBy("date","desc");
+	echo $pb->doSearchUrl();
+	
 	
